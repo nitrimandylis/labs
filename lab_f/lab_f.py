41443ff14688f947ffac7a868f4191f7380e9c76
vangelDI <evdiorinos@cgs.edu.gr>
2025-01-15 23:25:15 +0200

    Wee 2 in one day lets a go (i like games and anime)

--- lab_f/lab_f.py
+++ lab_f/lab_f.py
@@ -1,191 +1,183 @@
-"""
-MIT BWSI Autonomous RACECAR
-MIT License
-racecar-neo-outreach-labs
-
-File Name: lab_f.py
-
-Title: Lab F - Line Follower
-
-Author: [PLACEHOLDER] << [Write your name or team name here]
-
-Purpose: Write a script to enable fully autonomous behavior from the RACECAR. The
-RACECAR should automatically identify the color of a line it sees, then drive on the
-center of the line throughout the obstacle course. The RACECAR should also identify
-color changes, following colors with higher priority than others. Complete the lines 
-of code under the #TODO indicators to complete the lab.
-
-Expected Outcome: When the user runs the script, they are able to control the RACECAR
-using the following keys:
-- When the right trigger is pressed, the RACECAR moves forward at full speed
-- When the left trigger is pressed, the RACECAR, moves backwards at full speed
-- The angle of the RACECAR should only be controlled by the center of the line contour
-- The RACECAR sees the color RED as the highest priority, then GREEN, then BLUE
-"""
-
-########################################################################################
-# Imports
-########################################################################################
-
 import sys
 import cv2 as cv
 import numpy as np
 
-# If this file is nested inside a folder in the labs folder, the relative path should
-# be [1, ../../library] instead.
+# Import the racecar_core and racecar_utils libraries, which provide functionality for interacting with the racecar
 sys.path.insert(1, "../../library")
 import racecar_core
 import racecar_utils as rc_utils
 
-########################################################################################
-# Global variables
-########################################################################################
-
+# Create a racecar object, which is used to interact with the physical racecar
 rc = racecar_core.create_racecar()
 
-# >> Constants
-# The smallest contour we will recognize as a valid contour
+# Define the minimum contour area required for a contour to be considered valid
 MIN_CONTOUR_AREA = 30
 
-# A crop window for the floor directly in front of the car
+# Define the crop region for the camera image, which is used to focus on the floor in front of the racecar
 CROP_FLOOR = ((360, 0), (rc.camera.get_height(), rc.camera.get_width()))
 
-# TODO Part 1: Determine the HSV color threshold pairs for GREEN and RED
-# Colors, stored as a pair (hsv_min, hsv_max) Hint: Lab E!
-BLUE = ((90, 50, 50), (120, 255, 255))  # The HSV range for the color blue
-GREEN = _____  # The HSV range for the color green
-RED = _____  # The HSV range for the color red
+# Define a list of color ranges to search for in the camera image
+COLORS = [
+    # Red color range
+    ((0, 80, 80),     (10, 255, 255)),   
+    # Blue color range
+    ((107, 81, 80),   (85, 255, 255)),   
+    # Green color range
+    ((100, 234, 150), (104, 255, 170)),  
+    # Yellow color range
+    ((90, 80, 80),    (120, 255, 255))   
+]
 
-# Color priority: Red >> Green >> Blue
-COLOR_PRIORITY = (RED, GREEN, BLUE)
-
-# >> Variables
-speed = 0.0  # The current speed of the car
-angle = 0.0  # The current angle of the car's wheels
-contour_center = None  # The (pixel row, pixel column) of contour
-contour_area = 0  # The area of contour
+# Initialize variables to store the speed, angle, contour center, and contour area
+speed = 0.0  
+angle = 0.0  
+contour_center = None  
+contour_area = 0  
 
 
-########################################################################################
-# Functions
-########################################################################################
-
-# [FUNCTION] Finds contours in the current color image and uses them to update 
-# contour_center and contour_area
+# Define a function to update the contour information based on the current camera image
 def update_contour():
+    """
+    Updates the contour information based on the current camera image.
+
+    This function captures the current camera image, crops it to focus on the floor,
+    and then searches for contours within the specified color ranges. If a contour
+    is found, its center and area are calculated and stored.
+    """
     global contour_center
     global contour_area
 
+    # Capture the current camera image
     image = rc.camera.get_color_image()
 
+    # If the image is None, reset the contour information
     if image is None:
         contour_center = None
         contour_area = 0
     else:
-        # Crop the image to the floor directly in front of the car
-        image = rc_utils.crop(image, _____, _____)
+        # Crop the image to focus on the floor
+        image = rc_utils.crop(image, CROP_FLOOR[0], CROP_FLOOR[1])
 
-        # TODO Part 2: Search for line colors, and update the global variables
-        # contour_center and contour_area with the largest contour found
+        # Initialize the contour variable
+        contour = None
 
-        # Display the image to the screen
+        # Iterate over the color ranges and search for contours
+        for testingColor in COLORS:
+            # Find contours within the current color range
+            contours = rc_utils.find_contours(image, testingColor[0], testingColor[1])
+
+            # If contours are found, get the largest contour and break out of the loop
+            if contours:
+                contour = rc_utils.get_largest_contour(contours, MIN_CONTOUR_AREA)
+                break    
+
+        # If a contour is found, calculate its center and area
+        if contour is not None:
+            contour_center = rc_utils.get_contour_center(contour)
+            contour_area = rc_utils.get_contour_area(contour)
+
+            # Draw the contour and its center on the image
+            rc_utils.draw_contour(image, contour)
+            rc_utils.draw_circle(image, contour_center)
+
+        # If no contour is found, reset the contour information
+        else:
+            contour_center = None
+            contour_area = 0
+
+        # Display the updated image
         rc.display.show_color_image(image)
 
-# [FUNCTION] The start function is run once every time the start button is pressed
+
+# Define a function to initialize the racecar's speed and angle
 def start():
+    """
+    Initializes the racecar's speed and angle.
+
+    This function sets the initial speed and angle of the racecar to 0,
+    and then sets the update slow time to 0.5 seconds.
+    """
     global speed
     global angle
 
-    # Initialize variables
     speed = 0
     angle = 0
 
-    # Set initial driving speed and angle
+    # Set the initial speed and angle of the racecar
     rc.drive.set_speed_angle(speed, angle)
 
-    # Set update_slow to refresh every half second
+    # Set the update slow time to 0.5 seconds
     rc.set_update_slow_time(0.5)
 
-    # Print start message
-    print(
-        ">> Lab 2A - Color Image Line Following\n"
-        "\n"
-        "Controls:\n"
-        "   Right trigger = accelerate forward\n"
-        "   Left trigger = accelerate backward\n"
-        "   A button = print current speed and angle\n"
-        "   B button = print contour center and area"
-    )
 
-# [FUNCTION] After start() is run, this function is run once every frame (ideally at
-# 60 frames per second or slower depending on processing speed) until the back button
-# is pressed  
+# Define a function to update the racecar's speed and angle based on the current contour information
 def update():
     """
-    After start() is run, this function is run every frame until the back button
-    is pressed
+    Updates the racecar's speed and angle based on the current contour information.
+
+    This function updates the contour information, and then calculates the new speed
+    and angle based on the contour center and area. It also checks for controller input
+    to adjust the speed and angle.
     """
     global speed
     global angle
 
-    # Search for contours in the current color image
+    # Update the contour information
     update_contour()
 
-    # TODO Part 3: Determine the angle that the RACECAR should receive based on the current 
-    # position of the center of line contour on the screen. Hint: The RACECAR should drive in
-    # a direction that moves the line back to the center of the screen.
-
-    # Choose an angle based on contour_center
-    # If we could not find a contour, keep the previous angle
+    # If a contour is found, calculate the new angle based on the contour center
     if contour_center is not None:
-        angle = _____
+        angle = contour_center[1] - rc.camera.get_width() / 2
+        angle /= rc.camera.get_width() / 2
 
-    # Use the triggers to control the car's speed
-    rt = rc.controller.get_trigger(rc.controller.Trigger.RIGHT)
-    lt = rc.controller.get_trigger(rc.controller.Trigger.LEFT)
-    speed = rt - lt
+    # Get the forward and backward speed from the controller
+    forwardSpeed = rc.controller.get_trigger(rc.controller.Trigger.RIGHT)
+    backSpeed = rc.controller.get_trigger(rc.controller.Trigger.LEFT)
 
+    # Calculate the new speed based on the forward and backward speed
+    speed = forwardSpeed - backSpeed
+
+    # Set the new speed and angle of the racecar
     rc.drive.set_speed_angle(speed, angle)
 
-    # Print the current speed and angle when the A button is held down
+    # Check for controller input to print the speed and angle
     if rc.controller.is_down(rc.controller.Button.A):
         print("Speed:", speed, "Angle:", angle)
 
-    # Print the center and area of the largest contour when B is held down
+    # Check for controller input to print the contour information
     if rc.controller.is_down(rc.controller.Button.B):
         if contour_center is None:
             print("No contour found")
         else:
             print("Center:", contour_center, "Area:", contour_area)
 
-# [FUNCTION] update_slow() is similar to update() but is called once per second by
-# default. It is especially useful for printing debug messages, since printing a 
-# message every frame in update is computationally expensive and creates clutter
+
+# Define a function to update the slow information
 def update_slow():
     """
-    After start() is run, this function is run at a constant rate that is slower
-    than update().  By default, update_slow() is run once per second
+    Updates the slow information.
+
+    This function checks if a camera image is available, and if so, prints the contour information.
+    If no contour is found, it prints a message indicating that no contour was found.
     """
-    # Print a line of ascii text denoting the contour area and x-position
+    # Check if a camera image is available
     if rc.camera.get_color_image() is None:
-        # If no image is found, print all X's and don't display an image
         print("X" * 10 + " (No image) " + "X" * 10)
     else:
-        # If an image is found but no contour is found, print all dashes
+        # If no contour is found, print a message indicating that no contour was found
         if contour_center is None:
             print("-" * 32 + " : area = " + str(contour_area))
 
-        # Otherwise, print a line of dashes with a | indicating the contour x-position
+        # If a contour is found, print the contour information
         else:
             s = ["-"] * 32
             s[int(contour_center[1] / 20)] = "|"
             print("".join(s) + " : area = " + str(contour_area))
 
 
-########################################################################################
-# DO NOT MODIFY: Register start and update and begin execution
-########################################################################################
-
+# Check if this script is being run as the main program
 if __name__ == "__main__":
+    # Set the start, update, and update slow functions for the racecar
     rc.set_start_update(start, update, update_slow)
+    # Start the racecar
     rc.go()
