355209d7c2d3d0718ef21b12c67d91ba40f7b10f
vangelDI <evdiorinos@cgs.edu.gr>
2025-02-12 16:23:39 +0200

    helappaofhjksekba

--- lab_h/Lab_h.py
+++ lab_h/Lab_h.py
@@ -35,8 +35,7 @@ class Current_Cone(Enum):
     BLUE = 1
     NOTHING = 3
 
-
-cur_state = State.search
+cur_state = State.red
 Next_Cone = Next_Cone.NOTHING
 current_Cone = Current_Cone.NOTHING
 
@@ -52,12 +51,20 @@ def update_contours(image,image_depth):
     global current_Cone
     global Distance_Cone_Red
     global Distance_Cone_Blue
+    global contour_center
+    global contour_area
+    global contour_red
+    global contour_blue
+    global contour_center_red
+    global contour_center_blue
+    global MIN_CONTOUR_AREA
 
     MIN_CONTOUR_AREA = 800
 
     if image is None:
         contour_center = None
         contour_area = 0
+        print("ERROR No image")
     else:
         # Find all of the contours
         contours_red = rc_utils.find_contours(image, RED[0], RED[1])
@@ -71,24 +78,10 @@ def update_contours(image,image_depth):
         contour_center_red = rc_utils.get_contour_center(contour_red)
         contour_center_blue = rc_utils.get_contour_center(contour_blue)
 
-        if contour_red is not None and contour_blue is None:
-            cur_state = State.red
-            Next_Cone = Next_Cone.BLUE
-            contour_center = rc_utils.get_contour_center(contour_red)
-            cone_Selected = Current_Cone.RED
-            contour_area = rc_utils.get_contour_area(contour_red)
+        rc_utils.draw_contour(image, contour_red)
+        rc_utils.draw_contour(image, contour_blue)
 
-            return (cone_Selected, contour_center, contour_area, Next_Cone, contour_red)
-
-
-        if contour_blue is not None and contour_red is None:
-            cur_state = State.blue
-            Next_Cone = Next_Cone.RED
-            contour_center = rc_utils.get_contour_center(contour_blue)
-            contour_area = rc_utils.get_contour_area(contour_blue)
-            cone_Selected = Current_Cone.BLUE
-
-            return (cone_Selected, contour_center, contour_area, Next_Cone, contour_blue)
+        rc.display.show_color_image(image)
 
         if image_depth is not None:
 
@@ -96,31 +89,53 @@ def update_contours(image,image_depth):
                 Distance_Cone_Red = image_depth[contour_center_red[0]][contour_center_red[1]]
             if contour_blue is not None:
                 Distance_Cone_Blue = image_depth[contour_center_blue[0]][contour_center_blue[1]]
+                            
+        if contour_red is not None and contour_blue is None:
             
+            cur_state = State.red
+            Next_Cone = Next_Cone.BLUE
+            contour_center = rc_utils.get_contour_center(contour_red)
+            cone_Selected = Current_Cone.RED
+            print("cur_state = State.red")
+            contour_area = rc_utils.get_contour_area(contour_red)
 
+            return (cone_Selected, contour_center, contour_area, Next_Cone, contour_red)
+
+       
+
+
+        if contour_blue is not None and contour_red is None:
+            
+            cur_state = State.blue
+            Next_Cone = Next_Cone.RED
+            contour_center = rc_utils.get_contour_center(contour_blue)
+            contour_area = rc_utils.get_contour_area(contour_blue)
+            cone_Selected = Current_Cone.BLUE
+            print("cur_state = State.blue")
+            return (cone_Selected, contour_center, contour_area, Next_Cone, contour_blue)
+
+       
 
         if contour_red is not None and contour_blue is not None:
             if Distance_Cone_Red < Distance_Cone_Blue:
+              
                 contour_center = rc_utils.get_contour_center(contour_red)
                 contour_area = rc_utils.get_contour_area(contour_red)
                 cone_Selected = Current_Cone.RED
-                cur_state = State.red
+                cur_state = State.red  
+                print("cur_state = State.red")
                 return (cone_Selected, contour_center, contour_area, Next_Cone, contour_red)
             else:
+               
                 contour_center = rc_utils.get_contour_center(contour_blue)
                 contour_area = rc_utils.get_contour_area(contour_blue)
                 cone_Selected = Current_Cone.BLUE
-                cur_state = State.blue
-                return (cone_Selected, contour_center, contour_area, Next_Cone, contour_blue)
-    
-    current_time += rc.get_delta_time()
-    if int(current_time) % 2 == 0:
-        rc.display.show_color_image(image)
-        print("clorImage"+ f"current time: {current_time}")
-    if int(current_time) % 2 == 1:
-        rc.display.show_depth_image(depth_image)
-        print("depthImage" + f"current time: {current_time}")
+                cur_state = State.blue 
+            print("cur_state = State.blue")
+            return (cone_Selected, contour_center, contour_area, Next_Cone, contour_blue)
 
+       
+    
 def start():
     pass
 
@@ -141,7 +156,7 @@ def update():
         rc.drive.set_speed_angle(1, Random_angle)
 
     if cur_state == State.red:
-        Angle_error_blue = (contour_center_blue[1] - 320) / 320
+        Angle_error_blue = (contour_center_red[1] - 320) / 320
         angle = 0.5 * Angle_error_blue
         rc.drive.set_speed_angle(1,angle)
         if Distance_Cone_Red < CloseDistance:
@@ -150,7 +165,7 @@ def update():
         
 
         
-
+    print(f"cur_state: {cur_state}")
 
 def update_slow():
     pass
